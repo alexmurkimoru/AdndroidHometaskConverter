ad74a96601d1c7df4220d2b6a703e187d82f6291
alexmurkimoru <sashmanzheeva@gmail.com>
2023-11-18 03:50:33 +0300

    Sasha help me

--- app/src/main/java/com/example/converter/MainActivity.kt
+++ app/src/main/java/com/example/converter/MainActivity.kt
@@ -2,10 +2,48 @@ package com.example.converter
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.view.LayoutInflater
+import android.widget.ArrayAdapter
+import android.widget.RadioButton
+import android.widget.RadioGroup
+import android.widget.Spinner
+import com.example.converter.databinding.ActivityMainBinding
 
 class MainActivity : AppCompatActivity() {
+    private lateinit var bindingClass: ActivityMainBinding
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_main)
+        bindingClass = ActivityMainBinding.inflate(layoutInflater)
+        setContentView(bindingClass.root)
+        with(bindingClass) {
+
+
+            *lengthButton.setOnCheckedChangeListener { buttonView, isChecked ->
+                output.text = "Len"
+                spinnerConnection(inputSpinner, R.array.lenNames)
+            }
+
+            weightButton.setOnCheckedChangeListener { buttonView, isChecked ->
+                output.text = "weight"
+            }
+
+            bindingClass.currencyButton.setOnCheckedChangeListener { buttonView, isChecked ->
+                output.text = "currency"
+            }
+
+        }
+    }
+
+    private fun spinnerConnection(spinner: Spinner, textArrayResId : Int){//функция для подключения строкового массива в спиннер
+        ArrayAdapter.createFromResource(
+            this,
+            textArrayResId,
+            android.R.layout.simple_spinner_item
+        ).also { adapter ->
+            adapter.setDropDownViewResource(android.R.layout.simple_spinner_dropdown_item)
+            spinner.adapter = adapter
+        }
+
     }
 }
